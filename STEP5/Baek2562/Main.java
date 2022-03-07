9c2b6f0e68f4b62bfe5ac9e9d329656b6989db93
Dain-K <81922587+Dain-K@users.noreply.github.com>
2022-03-07 20:58:25 +0900

    2022-03-07

---
+++ STEP5/Baek2562/Main.java
@@ -0,0 +1,23 @@
+package STEP5.Baek2562;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[9];
+        int max = 0, line = 0;
+        for (int i = 0; i < 9; i++) {
+            arr[i] = sc.nextInt();
+            if (max < arr[i]) {
+                max = arr[i];
+                line = i + 1;
+            }
+        }
+
+        System.out.println(max + "\n" + line);
+        sc.close();
+    }
+
+}
