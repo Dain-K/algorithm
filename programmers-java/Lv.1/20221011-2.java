b2dddcacb2faba457f7d78a0cd9bf47107ff1fe3
Dain-K <dksudi76@naver.com>
2022-10-11 11:42:34 +0900

    문자열 내 p와 y의 개수

---
+++ programmers-java/Lv.1/20221011-2.java
@@ -0,0 +1,20 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        int cnt_p = 0;
+        int cnt_y = 0;
+
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == 'p'){
+                cnt_p++;
+            }
+            else if(s.charAt(i) == 'y') {
+                cnt_y++;
+            }
+        }
+        if (cnt_p == cnt_y) answer = true;
+        else answer = false;
+
+        return answer;
+    }
+}
