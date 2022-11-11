e9304dcf8a4526d8bfe482f2b66feebc6e13b1df
Dain-K <dksudi76@naver.com>
2022-11-11 16:06:51 +0900

    최댓값과 최솟값

---
+++ programmers-java/Lv.2/20221111-1.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] sNum = s.split(" ");
+        int[] num = new int[sNum.length];
+        
+        for(int i = 0; i < sNum.length; i++){
+            num[i] = Integer.valueOf(sNum[i]);
+        }
+        Arrays.sort(num);
+        
+        answer += num[0] + " " + num[sNum.length-1];
+        
+        return answer;
+    }
+}
