fa75608f1eba3196b11fcf45498f49240317e0fe
Dain-K <dksudi76@naver.com>
2022-11-20 17:03:34 +0900

    구명보트

---
+++ programmers-java/Lv.2/20221120-2.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);  
+        int min = 0;
+        for(int i = people.length - 1; i >= min; i--){
+            if(people[i] + people[min] <= limit){
+                answer++;
+                min++;
+            }
+            else answer++;
+        }
+        return answer;
+    }
+}
