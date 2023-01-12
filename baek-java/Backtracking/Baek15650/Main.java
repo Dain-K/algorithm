a8daef265dc929f89d1fdc437bf7cc6e753dae47
Dain-K <dksudi76@naver.com>
2023-01-12 17:44:40 +0900

    15650번: N과 M(2)

---
+++ baek-java/Backtracking/Baek15650/Main.java
@@ -0,0 +1,43 @@
+package STEP13.Baek15650;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int[] arr;
+    public static int N, M;
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        N = sc.nextInt();
+        M = sc.nextInt();
+
+        arr = new int[M];
+
+        DFS(1, 0);
+
+        sc.close();
+    }
+
+    public static void DFS(int at, int depth) {
+
+        if (depth == M) {
+
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+
+        }
+
+        for (int i = at; i <= N; i++) {
+
+            arr[depth] = i;
+            DFS(i + 1, depth + 1);
+
+        }
+    }
+}
