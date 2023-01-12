ada93f7d27e7e58de0c6e54c847f0efda03470a4
Dain-K <dksudi76@naver.com>
2023-01-12 17:24:42 +0900

    1157번: 단어공부

---
+++ baek-java/String/Baek1157/Main.java
@@ -0,0 +1,42 @@
+package STEP6.Baek1157;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		String small_alpa = "abcdefghijklmnopqrstuvwxyz";
+		String big_alpa = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int[] n = new int[small_alpa.length()];
+
+		for (int i = 0; i < small_alpa.length(); i++) {
+			for (int j = 0; j < s.length(); j++) {
+				if (small_alpa.charAt(i) == s.charAt(j))
+					n[i] += 1;
+			}
+		}
+		for (int i = 0; i < big_alpa.length(); i++) {
+			for (int j = 0; j < s.length(); j++) {
+				if (big_alpa.charAt(i) == s.charAt(j))
+					n[i] += 1;
+			}
+		}
+
+		int compare = 0;
+		char result = ' ';
+		for (int i = 0; i < big_alpa.length(); i++) {
+			if (compare < n[i]) {
+				compare = n[i];
+				result = big_alpa.charAt(i);
+			} else if (compare == n[i])
+				result = '?';
+		}
+		System.out.println(result);
+
+		sc.close();
+
+	}
+
+}
