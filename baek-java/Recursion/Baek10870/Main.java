54ac5f470110535f695b72a63419bf2c2b2b2282
Dain-K <dksudi76@naver.com>
2023-01-13 13:49:26 +0900

    10870번: 피보나치 수5

---
+++ baek-java/Recursion/Baek10870/Main.java
@@ -0,0 +1,22 @@
+package Recursion.Baek10870;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int[] result = new int[21];
+		result[0] = 0;
+		result[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+			result[i] = result[i - 1] + result[i - 2];
+		}
+		System.out.println(result[n]);
+		sc.close();
+	}
+
+}
