ab7e28316edd785ca307bf2ff7925a5b44a845c7
Dain-K <dksudi76@naver.com>
2023-01-13 13:43:04 +0900

    2798번: 블랙잭

---
+++ baek-java/BruteForce/Baek2798/Main.java
@@ -0,0 +1,44 @@
+package BruteForce.Baek2798;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int sum = sc.nextInt();
+		int[] num = new int[N];
+		for (int i = 0; i < N; i++) {
+			num[i] = sc.nextInt();
+		}
+
+		int MySum = 0, result = 0;
+
+		for (int i = 0; i < N - 2; i++) {
+			for (int j = 1; j < N - 1; j++) {
+				for (int z = 2; z < N; z++) {
+					if (i == j || i == z || (i == j && j == z) || j == z) {
+						continue;
+					}
+					MySum = num[i] + num[j] + num[z];
+					// System.out.print(num[i]+ " ");
+					// System.out.print(num[j]+ " ");
+					// System.out.println(num[z]);
+					if (sum >= MySum) {
+						if (MySum > result) {
+							result = MySum;
+
+						}
+					}
+				}
+			}
+		}
+
+		System.out.println(result);
+
+		sc.close();
+	}
+
+}
