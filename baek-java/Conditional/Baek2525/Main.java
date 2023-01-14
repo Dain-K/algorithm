fa78c10c650d2f849291bc96217522e46d921217
Dain-K <dksudi76@naver.com>
2023-01-14 15:27:19 +0900

    2525번: 오븐 시계

---
+++ baek-java/Conditional/Baek2525/Main.java
@@ -0,0 +1,26 @@
+package Conditional.Baek2525;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int A, B, C;
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt();
+        C = sc.nextInt();
+        if (B + C < 60) {
+            System.out.println(A);
+            System.out.println(B + C);
+        } else {
+            if (A + (B + C) / 60 > 23)
+                A = (A + (B + C) / 60) - 24;
+            else
+                A = A + (B + C) / 60;
+            B = (B + C) % 60;
+            System.out.println(A);
+            System.out.println(B);
+        }
+        sc.close();
+    }
+}
