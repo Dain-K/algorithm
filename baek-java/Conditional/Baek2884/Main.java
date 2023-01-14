23631ba8219d84808146fdeff7a9581016216f7a
Dain-K <dksudi76@naver.com>
2023-01-14 15:28:47 +0900

    2884번: 알람 시계

---
+++ baek-java/Conditional/Baek2884/Main.java
@@ -0,0 +1,26 @@
+package Conditional.Baek2884;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int H, M;
+        Scanner sc = new Scanner(System.in);
+        H = sc.nextInt();
+        M = sc.nextInt();
+        if (M >= 45) {
+            System.out.println(H);
+            System.out.println(M - 45);
+        } else {
+            if (H == 0)
+                H = 23;
+            else
+                H = H - 1;
+            M = 60 - (45 - M);
+            System.out.println(H);
+            System.out.println(M);
+        }
+        sc.close();
+    }
+
+}
