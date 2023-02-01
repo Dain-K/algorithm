190918eadc4911eece32145ef1f0ee1c5ba612c0
Dain-K <dksudi76@naver.com>
2023-02-01 21:35:37 +0900

    해시

---
+++ programmers-java/Lv.2/위장/Solution.java
@@ -0,0 +1,25 @@
+package 위장;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < clothes.length; i++){
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+        
+        Set<String> keySet = map.keySet();
+        
+        for(String key: keySet) {
+            answer *= map.get(key) + 1;
+        }
+        
+        
+        return answer - 1;
+    }
+}
