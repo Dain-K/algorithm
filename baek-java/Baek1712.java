a3c1f873daf8bf73b96010adc9355eb808e3217c
Dain-K <dksudi76@naver.com>
2023-03-03 19:57:34 +0900

    손익분기점

--- baek-java/Baek1712.java
+++ baek-java/Baek1712.java
@@ -1,8 +1,6 @@
-package BasicMath1.Baek1712;
-
 import java.util.*;
 
-public class Main {
+public class Baek1712 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
