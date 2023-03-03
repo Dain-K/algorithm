6d7a21e6f8c67fcfe817931138b3297dc2f9eb53
Dain-K <dksudi76@naver.com>
2023-03-03 20:49:29 +0900

    소인수분해

---
+++ baek-java/약수,배수와_소수/Baek11653/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main
+{
+	
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int N = Integer.parseInt(br.readLine());
+        int num = N;
+        while(true) {
+        	for(int i = 2; i <= N; i++) {
+        		if(num % i == 0) {
+        			pq.add(i);
+        			num = num / i;
+        			break;
+        		}
+        	}
+        	if(num == 1) break;
+        }
+        while(!pq.isEmpty()) System.out.println(pq.poll());
+    }
+    
+}
