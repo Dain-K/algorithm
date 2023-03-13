9e9642e56844e35d77577d2d9f5092d434027d65
Dain-K <dksudi76@naver.com>
2023-03-13 15:35:35 +0900

    퀵정렬

---
+++ jungol/정올_1146_선택정렬/정올_1146_선택정렬_김다인.java
@@ -0,0 +1,36 @@
+package 정올_1146_선택정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 정올_1146_선택정렬_김다인 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		for(int i = 0; i < N - 1; i++) {
+			int min = i;
+			for(int j = i + 1; j < N; j++) {
+				if(arr[j] < arr[min]) {
+					min = j;
+				}
+			}
+			swap(arr, i, min);
+			for(int idx: arr)System.out.print(idx + " ");
+			System.out.println();
+		}
+	}
+	
+	public static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+}
