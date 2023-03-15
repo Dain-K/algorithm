cd57e8cb9a810975f2dc5c6154c8933e3624fe84
Dain-K <dksudi76@naver.com>
2023-03-15 21:00:57 +0900

    15655번: N과 M (6)

--- baek-java/Backtracking/Baek15655/Main.java
+++ baek-java/Backtracking/Baek15655/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+
 public class Main {
 	static int N, M;
 	static int[] num;
