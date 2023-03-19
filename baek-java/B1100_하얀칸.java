f6af0a8e86ee8a08f6def5e6b9e2a1b1464a30cc
Dain-K <dksudi76@naver.com>
2023-03-19 23:19:33 +0900

    1100번: 하얀 칸

---
+++ baek-java/B1100_하얀칸.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+public class B1100_하얀칸 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = 0;
+		for(int i = 0; i < 8; i++) {
+			String s = br.readLine();
+			if(i % 2 != 0) {
+				for(int j = 0; j < 8; j++) {
+					if(j % 2 != 0 && s.charAt(j) == 'F') cnt++;
+				}
+			} else {
+				for(int j = 0; j < 8; j++) {
+					if(j % 2 == 0 && s.charAt(j) == 'F') cnt++;
+				}
+			}
+		}
+		System.out.println(cnt);
+	}
+}
