9c322e666459ce624cf375743ad7fefce846ffbb
Dain-K <dksudi76@naver.com>
2023-03-20 20:58:59 +0900

    11653번: 소인수분해

--- baek-java/Math/B11653_소인수분해.java
+++ baek-java/Math/B11653_소인수분해.java
@@ -1,9 +1,10 @@
+package Math;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
-public class Main
+public class B11653_소인수분해
 {
 	
     public static void main(String args[]) throws IOException
